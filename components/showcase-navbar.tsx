9bd8dd8000d1198547cc78a3fbebb72473618e55
Kapil <you@example.com>
2024-08-29 15:05:53 +0530

    number change

--- components/showcase-navbar.tsx
+++ components/showcase-navbar.tsx
@@ -67,7 +67,6 @@ xl:w-4/5
       <div className="flex items-center gap-x-4">
       <a href="tel:5193191562" className="hidden xl:flex">
               <button className="px-4 py-2  rounded-md flex items-center gap-x-3 ">
-            9411867984
               </button>
             </a>
 
