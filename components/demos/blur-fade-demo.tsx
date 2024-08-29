15688f319ac42ade111de002adb9ff45fc1ecb32
Kapil <you@example.com>
2024-08-29 12:28:10 +0530

    final

--- components/demos/blur-fade-demo.tsx
+++ components/demos/blur-fade-demo.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const works = [
   {
     background: "bg-gray-200",
-    imageUrl: "/images/Beyond.png",
+    imageUrl: "/images/Beyoond.png",
     title: "Beyond Water",
     link: "https://youtube.com/shorts/u-nes3788II?si=_i5pdh8Avp9LWzHF",
   },
