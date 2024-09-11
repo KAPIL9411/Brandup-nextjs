994e6986f48cdc067a261a994d44dc480a3da724
Kapil <you@example.com>
2024-09-11 21:56:36 +0530

    speed

--- components/ui/infinite-moving-logos.tsx
+++ components/ui/infinite-moving-logos.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export const InfiniteMovingLogos = ({
   items,
   direction = "left",
-  speed = "fast",
+  speed = "slow",
   pauseOnHover = true,
   className,
 }: {
